7bb2acdd3cf1b6f0fcf30f5d953186a77c47261b
wildwonderwinsto <wildwonderwinston@gmail.com>
2025-12-03 21:43:12 -0600

    revert(steelworks)

--- utils/DogeTransport.ts
+++ utils/DogeTransport.ts
@@ -1,4 +1,4 @@
-export const DOGE_BASE_URL = "https://wintonswebsiteproxy.onrender.com/indev";
+export const DOGE_BASE_URL = "https://wintonswebsiteproxy.onrender.com";
 const PROXY_PREFIX = "/uv/service/";
 const PROXY_HOSTNAME = "wintonswebsiteproxy.onrender.com";
 
